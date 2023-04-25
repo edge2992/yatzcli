e6107f416f709627cad7120f106be71c16edabef
edge2992 <torune.summer@gmail.com>
2023-04-25 21:02:32 +0900

    feat(room_manager.go): add RoomManager struct and methods for creating, joining, and listing rooms.

---
+++ server/room_manager.go
@@ -0,0 +1,61 @@
+package server
+
+import (
+	"encoding/gob"
+	"errors"
+	"sync"
+	"yatzcli/game"
+)
+
+type RoomManager struct {
+	rooms map[string]*Room
+	mutex sync.RWMutex
+}
+
+func NewRoomManager() *RoomManager {
+	return &RoomManager{
+		rooms: make(map[string]*Room),
+	}
+}
+
+func (rm *RoomManager) CreateRoom(roomID string) (*Room, error) {
+	rm.mutex.Lock()
+	defer rm.mutex.Unlock()
+
+	if _, exists := rm.rooms[roomID]; exists {
+		return nil, errors.New("Room already exists")
+	}
+	newRoom := NewRoom(roomID)
+	rm.rooms[roomID] = newRoom
+	return newRoom, nil
+}
+
+func (rm *RoomManager) JoinRoom(roomID string, player *game.Player, encoder *gob.Encoder) (*Room, error) {
+	rm.mutex.Lock()
+	defer rm.mutex.Unlock()
+
+	room, exists := rm.rooms[roomID]
+	if !exists {
+		return nil, errors.New("Room does not exist")
+	}
+
+	if err := room.AddPlayer(player, encoder); err != nil {
+		return nil, err
+	}
+	return room, nil
+}
+
+func (rm *RoomManager) LeaveRoom() {
+	// TODO implement
+}
+
+func (rm *RoomManager) ListRooms() []*Room {
+	rm.mutex.RLock()
+	defer rm.mutex.RUnlock()
+
+	roomList := make([]*Room, 0, len(rm.rooms))
+	for _, room := range rm.rooms {
+		roomList = append(roomList, room)
+	}
+	return roomList
+}
